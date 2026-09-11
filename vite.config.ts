1894a1d667b79390c96b331570d61335f775f6c2
song0082 <148782599+song0082@users.noreply.github.com>
2026-09-12 01:01:41 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 import fs from "fs"
 
-import pkg from "./package.json"
 import { createHtmlPlugin } from "vite-plugin-html"
 import {
   GROOM_FULLNAME,
@@ -14,15 +13,6 @@ import {
 } from "./src/const"
 
 const distFolder = "build"
-
-let base = "/"
-
-try {
-  const url = new URL(pkg.homepage)
-  base = url.pathname
-} catch (e) {
-  base = pkg.homepage || "/"
-}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -51,5 +41,5 @@ export default defineConfig({
   ],
   server: { port: 3000 },
   build: { outDir: distFolder },
-  base,
+  base: "/wedding-invitation/",
 })
