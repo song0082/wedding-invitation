85dd51b85458b9c680cf7e5d2482a984ead5a427
song0082 <148782599+song0082@users.noreply.github.com>
2026-09-11 22:54:39 +0900

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -14,7 +14,7 @@ export { dayjs }
  * 예식 일시 설정
  * Asia/Seoul 타임존 기준으로 설정합니다.
  */
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2027-01-16 12:00", "Asia/Seoul")
 
 /**
  * 예식 일시 포맷
