e942b1c2a19e5a87a3e51df8a9104fd137117dd1
song0082 <148782599+song0082@users.noreply.github.com>
2026-09-12 13:55:17 +0900

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,54 +1,28 @@
-import coverImage from "./cover.jpg"
-import image1 from "./image1.jpg"
-import image2 from "./image2.jpg"
-import image3 from "./image3.jpg"
-import image4 from "./image4.jpg"
-import image5 from "./image5.jpg"
-import image6 from "./image6.jpg"
-import image7 from "./image7.jpg"
-import image8 from "./image8.jpg"
-import image9 from "./image9.jpg"
-import image10 from "./image10.jpg"
-import image11 from "./image11.jpg"
-import image12 from "./image12.jpg"
-import image13 from "./image13.jpg"
-import image14 from "./image14.jpg"
-import image15 from "./image15.jpg"
-import image16 from "./image16.jpg"
-import image17 from "./image17.jpg"
-import image18 from "./image18.jpg"
-import image19 from "./image19.jpg"
-import image20 from "./image20.jpg"
-import image21 from "./image21.jpg"
-
-/**
- * 메인 커버 이미지
- */
-export const COVER_IMAGE = coverImage
-
-/**
- * 갤러리에 표시될 이미지 목록
- */
-export const GALLERY_IMAGES = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
-  image13,
-  image14,
-  image15,
-  image16,
-  image17,
-  image18,
-  image19,
-  image20,
-  image21,
-]
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
+    />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:title"
+      content="<%= GROOM_FULLNAME %> ❤️ <%= BRIDE_FULLNAME %> 결혼합니다."
+    />
+    <meta property="og:image" content="./preview_image.png" />
+    <meta property="og:url" content="%BASE_URL%/" />
+    <meta property="og:description" content="<%= DESCRIPTION %>" />
+    <link rel="preload" href="./MapoGoldenPierA.woff" as="font" crossorigin />
+    <link rel="preload" href="./AlluraRegular.woff" as="font" crossorigin />
+    <link rel="manifest" href="./manifest.json" />
+    <link rel="icon" href="./favicon.ico" />
+    <link rel="stylesheet" href="./font.css" />
+    <title><%= GROOM_FULLNAME %> ❤️ <%= BRIDE_FULLNAME %> 결혼합니다.</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="./src/index.tsx"></script>
+  </body>
+</html>
