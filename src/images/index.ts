406824006aaf421743b0d95cbf8beece3eec4e4b
song0082 <148782599+song0082@users.noreply.github.com>
2026-09-13 01:50:43 +0900

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -4,22 +4,6 @@ import image2 from "./image2.jpg"
 import image3 from "./image3.jpg"
 import image4 from "./image4.jpg"
 import image5 from "./image5.jpg"
-import image6 from "./image6.jpg"
-import image7 from "./image7.jpg"
-import image8 from "./image8.jpg"
-import image9 from "./image9.jpg"
-import image10 from "./image10.jpg"
-import image11 from "./image11.jpg"
-import image12 from "./image12.jpg"
-import image13 from "./image13.jpg"
-import image14 from "./image14.jpg"
-import image15 from "./image15.jpg"
-import image16 from "./image16.jpg"
-import image17 from "./image17.jpg"
-import image18 from "./image18.jpg"
-import image19 from "./image19.jpg"
-import image20 from "./image20.jpg"
-import image21 from "./image21.jpg"
 
 /**
  * 메인 커버 이미지
@@ -35,20 +19,5 @@ export const GALLERY_IMAGES = [
   image3,
   image4,
   image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
-  image13,
-  image14,
-  image15,
-  image16,
-  image17,
-  image18,
-  image19,
-  image20,
-  image21,
+  
 ]
